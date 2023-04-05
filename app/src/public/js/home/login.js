e769ce8a0b74cb9eb64998661c978817369cc0b5
2seongha <tjdgkwlwhs@naver.com>
2023-04-05 17:47:53 +0900

    dom을 이용하여 html객체 이용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("됨");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const login_button = document.querySelector("button");
+
+login_button.addEventListener("click", login);
+
+function login(){
+  const req = {
+    id: id.value,
+    password: password.value,
+  }
+  console.log(req);
+}
